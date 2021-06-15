de48288bf23c1e08768c88d1d458aba4fabb6f9b
yosyos36 <yosyos0306@gmail.com>
2021-06-16 01:28:24 +0900

    update: implement signup

--- src/pages/signup.vue
+++ src/pages/signup.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="container">
+  <div class="container text-center">
     <b-field label="Username" type="is-danger" message="This email is invalid">
-      <b-input type="email" maxlength="30"></b-input>
+      <b-input v-model="signupEmail" type="email" maxlength="30"></b-input>
     </b-field>
     <b-field label="Password">
-      <b-input type="password" password-reveal></b-input>
+      <b-input v-model="signupPass" type="password" password-reveal></b-input>
     </b-field>
+    <b-button v-on:click="signUp()">Button</b-button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      signupEmail: "",
+      signupPass: "",
+    };
+  },
+  methods: {
+    async signUp() {
+      try {
+        const { user } = await this.$fire.auth.createUserWithEmailAndPassword(
+          this.signupEmail,
+          this.signupPass
+        );
+        this.$router.push("/");
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+});
 </script>>
