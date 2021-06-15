ec88dda3bf38e96c82c0c7512c8d3df189e4780d
yosyos36 <yosyos0306@gmail.com>
2021-06-15 15:46:33 +0900

    add: buefy

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,9 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    'nuxt-buefy'
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
