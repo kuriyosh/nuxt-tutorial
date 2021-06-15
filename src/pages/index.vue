46eca03090c8246f127dda1577aaa6fb3b4ca2ba
yosyos36 <yosyos0306@gmail.com>
2021-06-15 14:17:10 +0900

    update: implement sample array

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,14 @@
 <template>
-  <div class="container">
+  <div class="flex content-center w-full justify-center center">
     <div>
       <Logo />
-      <h1 class="title">
+      <h1 class="title text-center mt-5">
         nuxt-tutorial
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <div class="mt-5">
+        <ul>
+        <li class="mt-1 text-center" v-for="item in testArray" :key="item">{{item}}</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -30,51 +17,9 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data: () => ({
+    testArray: ['test1', 'test2', 'test3']
+  })
+})
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
