35b0e721bba69df382d13d96aa6677279d3f7713
yosyos36 <yosyos0306@gmail.com>
2021-06-16 13:33:39 +0900

    update: formatter

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,12 +2,12 @@
   <div class="flex content-center w-full justify-center center">
     <div>
       <Logo />
-      <h1 class="title text-center mt-5">
-        nuxt-tutorial
-      </h1>
+      <h1 class="title text-center mt-5">nuxt-tutorial</h1>
       <div class="mt-5">
         <ul>
-        <li class="mt-1 text-center" v-for="item in testArray" :key="item">{{item}}</li>
+          <li class="mt-1 text-center" v-for="item in testArray" :key="item">
+            {{ item }}
+          </li>
         </ul>
       </div>
     </div>
@@ -15,11 +15,11 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
   data: () => ({
-    testArray: ['test1', 'test2', 'test3']
-  })
-})
+    testArray: ["test1", "test2", "test3"],
+  }),
+});
 </script>
