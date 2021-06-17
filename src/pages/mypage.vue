c690888b2a82d19a845cf312803be89e09fd56a2
yosyos36 <yosyos0306@gmail.com>
2021-06-17 15:07:10 +0900

    update: implement fetch sampleDatas query

--- src/pages/mypage.vue
+++ src/pages/mypage.vue
@@ -5,8 +5,14 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+import ALL_SAMPLE_DATA from "~/graphql/query/ALL_SAMPLE_DATA";
+
 export default Vue.extend({
-    middleware: 'auth'
-})
+  middleware: "auth",
+  async created() {
+    const datas = await this.$apollo.query({ query: ALL_SAMPLE_DATA });
+    console.log(datas);
+  },
+});
 </script>
